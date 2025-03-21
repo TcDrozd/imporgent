abe42a1475a6f8a8d4a163bab368273e287bc1f9
TcDrozd <trcharlesd@gmail.com>
2025-03-21 09:47:54 -0400

    Initial Commit

---
+++ Imporgent/ImporgentApp.swift
@@ -0,0 +1,20 @@
+//
+//  ImporgentApp.swift
+//  Imporgent
+//
+//  Created by Trevor Drozd on 3/21/25.
+//
+
+import SwiftUI
+
+@main
+struct ImporgentApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ ImporgentTests/ImporgentTests.swift
@@ -0,0 +1,17 @@
+//
+//  ImporgentTests.swift
+//  ImporgentTests
+//
+//  Created by Trevor Drozd on 3/21/25.
+//
+
+import Testing
+@testable import Imporgent
+
+struct ImporgentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
